41aca0981a8b10a97689500b9666b55966042de8
Nicola Quaia <144346869+nicolaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-10-11 09:43:31 +0200

    Add files via upload

---
+++ week 6_3/exercise 3/continuum2_plotdeformed_0001.m
@@ -0,0 +1,35 @@
+ % Plotting Un-Deformed and Deformed Structure
+ close all
+ clear all
+ continuum2_plotdeformed_data0001;
+ % Make plot
+ figure
+ set(gcf,'Name','Deformed')
+ subplot(2,1,2)
+ hold on
+ for e = 1:size(IX,1)
+     edof = [2*IX(e,1)-1 2*IX(e,1) 2*IX(e,2)-1 2*IX(e,2)...
+         2*IX(e,3)-1 2*IX(e,3) 2*IX(e,4)-1 2*IX(e,4)];
+     xx = X(IX(e,1:4),1) + D(edof(1:2:8));
+     yy = X(IX(e,1:4),2) + D(edof(2:2:8));
+     patch(xx,yy,[1 1 0]);
+ end
+ title('Deformed')
+ axis equal;
+ xaxes = get(gca,'xlim');
+ yaxes = get(gca,'ylim');
+ axis off;
+ subplot(2,1,1)
+ hold on
+ for e = 1:size(IX,1)
+     xx = X(IX(e,1:4),1);
+     yy = X(IX(e,1:4),2);
+     patch(xx,yy,[1 1 0]);
+ end
+ title('Undeformed')
+ axis([min(xaxes(1),min(X(:,1))) max(xaxes(2),max(X(:,1)))...
+  min(yaxes(1),min(X(:,2))) max(yaxes(2),max(X(:,2))) ]);
+ axis equal;
+ axis off;
+ hold off
+ set(gcf,'color',[ 1  1 1]);
