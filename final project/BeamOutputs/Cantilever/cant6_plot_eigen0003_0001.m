ab0306ce91551edb8e260534621149349adba93a
empem678 <145101905+empem678@users.noreply.github.com>
2023-12-01 16:44:22 +0100

    Add files via upload

---
+++ final project/BeamOutputs/Cantilever/cant6_plot_eigen0003_0001.m
@@ -0,0 +1,23 @@
+ % Plotting Element Values
+ close all
+ clear all
+ cant6_plot_eigen0003_data_0001;
+ % Determine colorscale
+ colormap('jet')
+ cmap = jet;
+ cinterp = linspace(min(plotval),max(plotval),size(cmap,1));
+ % Make plot
+ title('eigenvalue')
+ hold on
+ for e = 1:size(IX,1)
+    [dummy,arr_pos] = min(abs(cinterp-plotval(e)));
+    xx = X(IX(e,1:4),1);
+    yy = X(IX(e,1:4),2);
+    patch(xx,yy,cmap(arr_pos,:));
+ end
+ axis equal;
+ axis off;
+ caxis([min(plotval) max(plotval)]);
+ colorbar;
+ hold off
+ set(gcf,'color',[ 1  1 1]);
